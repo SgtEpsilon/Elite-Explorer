0d93e0434436e2d11f0113b75d0068bc680b7cd7
Epsilon Niner <80264751+SgtEpsilon@users.noreply.github.com>
2026-03-08 12:03:49 +0000

    Added mission tracker.

--- preload.js
+++ preload.js
@@ -13,6 +13,7 @@ contextBridge.exposeInMainWorld('electronAPI', {
   onLiveData:           (cb) => on('live-data',             cb),
   onProfileData:        (cb) => on('profile-data',          cb),
   onBodiesData:         (cb) => on('bodies-data',           cb),
+  onMissionsData:       (cb) => on('missions-data',         cb),
   onLocation:           (cb) => on('location-data',         cb),
   onProgress:           (cb) => on('scan-progress',         cb),
   onScanAll:            (cb) => on('scan-all-journals',     cb),
